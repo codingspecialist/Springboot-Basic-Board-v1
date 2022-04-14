c8d7ec123f53b978211ebae815ca81a54fabf098
ssar <ssar@nate.com>
2022-04-14 15:10:51 +0900

    글쓰기 완료

--- src/main/java/site/metacoding/board/web/BoardController.java
+++ src/main/java/site/metacoding/board/web/BoardController.java
@@ -3,9 +3,10 @@ package site.metacoding.board.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.board.domain.Board;
 import site.metacoding.board.domain.BoardRepository;
 
 @RequiredArgsConstructor
@@ -32,5 +33,11 @@ public class BoardController {
     @GetMapping("/board/updateForm")
     public String boardUpdateForm() {
         return "boardUpdateForm";
+    }
+
+    @PostMapping("/board")
+    public String boardWrite(Board board) { // title, content, nickname 받기
+        boardRepository.save(board);
+        return "redirect:/";
     }
 }
